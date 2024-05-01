abc69cce135b24efaab78ab7e2b33caf1a39c3b1
AndreaParrini <andrea.parrini26@gmail.com>
2024-05-01 20:58:04 +0200

    create partials header and footer

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,90 @@
+<footer>
+    <section class="section_links">
+        <div class="container">
+            <div class="row">
+                <div class="col-2">
+                    <div>
+                        <ul>
+                            <h4>Dc comics</h4>
+                            <li>Characters</li>
+                            <li>Comics</li>
+                            <li>Movies</li>
+                            <li>TV</li>
+                            <li>Games</li>
+                            <li>Videos</li>
+                            <li>News</li>
+                        </ul>
+                        <ul>
+                            <h4>Shop</h4>
+                            <li>Shop DC</li>
+                            <li>Shop Dc Collectibles</li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-2">
+                    <div>
+                        <ul>
+                            <h4>DC</h4>
+                            <li>Term Of Use</li>
+                            <li>Privacy policy (New)</li>
+                            <li>Ad Choices</li>
+                            <li>Advertising</li>
+                            <li>Jobs</li>
+                            <li>Subscriptions</li>
+                            <li>Talent Workshops</li>
+                            <li>CPSC Certificates</li>
+                            <li>Ratings</li>
+                            <li>Shop Help</li>
+                            <li>Contact Us</li>
+                        </ul>
+
+                    </div>
+                </div>
+                <div class="col-2">
+                    <div>
+                        <ul>
+                            <h4>Sites</h4>
+                            <li>DC</li>
+                            <li>MAD Magazine</li>
+                            <li>DC Kids</li>
+                            <li>DC Universe</li>
+                            <li>DC Power Visa</li>
+                        </ul>
+                    </div>
+                </div>
+
+            </div>
+            <section class="cookies-settings">
+                <div class="row">
+                    <div class="col-">
+                        All Site Content TM and 2020 DC Entertainment, unless otherwise <a href="">noted
+                            here</a>.All rights reserved.
+                        <div>
+                            <a href="">Cookies Settings</a>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+    </section>
+    <section class="section_follow">
+        <div class="container">
+            <div class="sign_and_follow">
+                <button class="btn">sign-up now!</button>
+                <div class="follow-us">
+                    <span>Follow Us</span>
+                    <a href=""><img src="{{ Vite::asset('resources/images/footer-facebook.png') }}"
+                            alt=""></a>
+                    <a href=""><img src="{{ Vite::asset('resources/images/footer-periscope.png') }}"
+                            alt=""></a>
+                    <a href=""><img src="{{ Vite::asset('resources/images/footer-pinterest.png') }}"
+                            alt=""></a>
+                    <a href=""><img src="{{ Vite::asset('resources/images/footer-twitter.png') }}"
+                            alt=""></a>
+                    <a href=""><img src="{{ Vite::asset('resources/images/footer-youtube.png') }}"
+                            alt=""></a>
+                </div>
+            </div>
+        </div>
+    </section>
+</footer>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,33 @@
+<header>
+    <div class="header-plus">
+        <div class="container">
+            <ul>
+                <li>Dc power visa<i class="fa-regular fa-registered"></i> </li>
+                <li>Addittional DC Sites<i class="fa-solid fa-caret-down"></i></li>
+            </ul>
+        </div>
+    </div>
+    <div class="container">
+        <nav>
+            <a href="/"><img src="{{ Vite::asset('resources/images/dc-logo.png') }}" alt="Logo"
+                    srcset=""></a>
+            <ul>
+                <li><a href="/">Characters</a></li>
+                <li class="{{ Route::currentRouteName() === 'comics' ? 'active' : '' }}"><a
+                        href="{{ Route('comics.index') }}">Comics</a></li>
+                <li><a href="/">Movies</a></li>
+                <li><a href="/">TV</a></li>
+                <li><a href="/">Games</a></li>
+                <li><a href="/">Collectibles</a></li>
+                <li><a href="/">Videos</a></li>
+                <li><a href="/">Fans</a></li>
+                <li><a href="/">News</a></li>
+                <li><a href="/">Shop<i class="fa-solid fa-caret-down"></i></a></li>
+            </ul>
+            <div>
+                <input type="text" name="" id="" placeholder="Search">
+                <button><i class="fa-solid fa-magnifying-glass fa-xl"></i></button>
+            </div>
+        </nav>
+    </div>
+</header>
